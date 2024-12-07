7f0f6bfc44fe1ee8c24cde785d29c7774095646d
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-12-07 12:55:03 +0100

    Update: Display hamburger menu on smaller screens and improve menu toggle functionality

--- components/header/MainHeader.js
+++ components/header/MainHeader.js
@@ -1,3 +1,6 @@
+//
+'use client';
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logoPng from "@/assets/logo.png";
@@ -6,6 +9,13 @@ import HeaderBackground from "./HeaderBackground.js";
 import NavLink from "./navLink.js";
 
 export default function MainHeader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prevState) => !prevState);
+    console.log("Menu open state:", !isMenuOpen);
+  };
+
   return (
     <>
       <HeaderBackground />
@@ -16,14 +26,40 @@ export default function MainHeader() {
         </Link>
 
         <nav className={classes.nav}>
-          <ul>
+          <ul className={`${isMenuOpen ? classes.hidden : ""}`}>
             <li>
               <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <NavLink href="/community">Foodies commmunity</NavLink>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
+
+          {/* Hamburger button */}
+          <div className={classes.hamburgerWrapper}>
+            <button
+              id="menu-btn"
+              type="button"
+              className={`${classes.hamburger} ${isMenuOpen ? classes.open : ""}`}
+              onClick={handleMenuToggle}
+            >
+              <span className={classes.hamburgerTop}></span>
+              <span className={classes.hamburgerMiddle}></span>
+              <span className={classes.hamburgerBottom}></span>
+            </button>
+          </div>
+
+          <div
+            id="menu"
+            className={`${classes.menu} ${isMenuOpen ? classes.flex : classes.hidden}`}
+          >{isMenuOpen && (
+            <>
+              <Link href="/meals" className={classes.noUnderline}>Browse Meals</Link>
+              <Link href="/community" className={classes.noUnderline}>Community</Link>
+            </>
+          )}
+            
+          </div>
         </nav>
       </header>
     </>
